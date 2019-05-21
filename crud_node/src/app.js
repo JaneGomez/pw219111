5e75d1bfe911607cea9a3a42fa462bee39c54e0d
JaneGomez <janegomezc@gmail.com>
2019-05-20 20:35:59 -0600

    Ejemplo Crud NodeJS MySQL

--- crud_node/src/app.js
+++ crud_node/src/app.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const express = require('express');
-const morgan = require('morgan');
+//const morgan = require('morgan');
 const mysql = require('mysql'); //sequel
 const myConnection = require('express-myconnection')
 const app = express();
@@ -11,22 +11,24 @@ const indiceRutas=require('./rutas/index');
 //Configuraciones
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname,'views'));
+app.set('views', path.join(__dirname,'vistas'))
 
-//Usamos las rutas
-app.use("/",indiceRutas);
 
 //Middleware 
-app.use(morgan('dev')); //dev = desarrollo
-app.use(myConnection(mysql,{
-	host: 'localhost',
-	user: 'root',
-	password: '',
-	port: 3306,
-	database: 'crudnodejsmysql'
-}, 'single'));
+// app.use(morgan('dev')); //dev = desarrollo
+ app.use(myConnection(mysql,{
+ 	host: 'localhost',
+ 	user: 'root',
+ 	password: '',
+ 	port: 3306,
+ 	database: 'crudnodejsmysql'
+ }, 'single'));
+ app.use(express.urlencoded({extended: false}));
+
+//Usamos las rutas
+app.use('/',indiceRutas);
 
 //Activar el servidor de escucha
 app.listen(app.get('port'), () => {
 	console.log("Escuchando en puerto 3000");
-})
+});
